fc538db17d32e7330eb529c294d1f382e2072f82
Raul Ismagilov <smaggy1337@gmail.com>
2025-05-11 18:41:53 +0300

    added assets

---
+++ Tracker/Tracker/SplashViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class SplashViewController: UIViewController {
+    private var splashLogoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI(){
+        
+    }
+}
